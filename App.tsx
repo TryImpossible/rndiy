2d096bacbba1e5444285bd39d43432be1fed1226
Barry Xie <barry.xie@antcosa.com>
2019-02-21 19:33:36 +0800

    test husky

--- App.tsx
+++ App.tsx
@@ -5,8 +5,8 @@
  * @format
  * @flow
  */
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -15,7 +15,7 @@ const instructions = Platform.select({
 
 export default class App extends Component {
   public render() {
-    console.log('sdfsfsd');
+    console.log('test husky');
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
