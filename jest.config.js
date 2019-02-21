e44d330d993974f2149256d66e1f54b198a09032
Barry Xie <barry.xie@antcosa.com>
2019-02-22 00:32:14 +0800

    代码规则

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(js)$': '<rootDir>/node_modules/babel-jest',
+    '\\.(ts|tsx)$': '<rootDir>/node_modules/ts-jest/preprocessor.js'
+  },
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  testPathIgnorePatterns: ['\\.snap$', '<rootDir>/node_modules/'],
+  cacheDirectory: '.jest/cache'
+};
